e2696c3b879c0209953968fbc21af49df090ebe0
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-12-27 19:18:06 +0700

    fix: exercise 1

---
+++ weather-forecast/weather_forecast.go
@@ -0,0 +1,10 @@
+// Package weather for Goblinocus forecast
+package weather
+
+var CurrentCondition string
+var CurrentLocation string
+
+func Forecast(city, condition string) string {
+	CurrentLocation, CurrentCondition = city, condition
+	return CurrentLocation + " - current weather condition: " + CurrentCondition
+}
