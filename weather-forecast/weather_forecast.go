b3bfeed1b8d49e67b81ce4ff914df40057d794e1
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-12-31 18:36:17 +0700

    finish

--- weather-forecast/weather_forecast.go
+++ weather-forecast/weather_forecast.go
@@ -7,6 +7,7 @@ var CurrentCondition string
 // CurrentLocation represent the current weather location for the Forecast Function.
 var CurrentLocation string
 
+// Forecast represent the weather condition of the given city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
