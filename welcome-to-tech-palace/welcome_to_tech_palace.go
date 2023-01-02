86fe39f6967d4044b88b5f716566b22caa60ad11
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2023-01-02 19:28:46 +0700

    second exercise

--- welcome-to-tech-palace/welcome_to_tech_palace.go
+++ welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -9,7 +9,7 @@ func WelcomeMessage(customer string) string {
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	return (strings.Repeat("*", numStarsPerLine) + "\n" + welcomeMsg + "\n" + strings.Repeat("*", numStarsPerLine))
 }
 
 // CleanupMessage cleans up an old marketing message.
