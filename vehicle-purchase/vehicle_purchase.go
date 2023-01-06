dbafd6824f4d291b5ab347b07f168fa9d0572eed
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2023-01-06 11:08:49 +0700

    finish

--- vehicle-purchase/vehicle_purchase.go
+++ vehicle-purchase/vehicle_purchase.go
@@ -10,10 +10,18 @@ func NeedsLicense(kind string) bool {
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	if option1 < option2 {
+		return option1 + " is clearly the better choice."
+	}
+	return option2 + " is clearly the better choice."
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+	if age < 3 {
+		return originalPrice * 0.8
+	} else if age >= 3 && age < 10 {
+		return originalPrice * 0.7
+	}
+	return originalPrice * 0.5
 }
