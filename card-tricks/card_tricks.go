25788cbf741da4672d7c4dde14ab5bed151910f7
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2023-01-10 14:26:42 +0700

    finish

--- card-tricks/card_tricks.go
+++ card-tricks/card_tricks.go
@@ -17,15 +17,23 @@ func GetItem(slice []int, index int) int {
 // SetItem writes an item to a slice at given position overwriting an existing value.
 // If the index is out of range the value needs to be appended.
 func SetItem(slice []int, index, value int) []int {
-	panic("Please implement the SetItem function")
+	if index >= len(slice) || index < 0 {
+		slice = append(slice, value)
+	} else {
+		slice[index] = value
+	}
+	return slice
 }
 
 // PrependItems adds an arbitrary number of values at the front of a slice.
 func PrependItems(slice []int, values ...int) []int {
-	panic("Please implement the PrependItems function")
+	return append(values, slice...)
 }
 
 // RemoveItem removes an item from a slice by modifying the existing slice.
 func RemoveItem(slice []int, index int) []int {
-	panic("Please implement the RemoveItem function")
+	if index > len(slice) || index < 0 {
+		return slice
+	}
+	return append(slice[:index], slice[index+1:]...)
 }
