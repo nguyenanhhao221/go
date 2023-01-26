0bd697b663190b6bddbf7820851ebb33fafa7b1b
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2023-01-26 17:20:03 +0700

    finish

--- elons-toys/elons_toys.go
+++ elons-toys/elons_toys.go
@@ -1,9 +1,30 @@
 package elon
 
+import "fmt"
+
 // TODO: define the 'Drive()' method
+func (car *Car) Drive() {
+	if car.battery < car.batteryDrain {
+		return
+	}
+	car.battery -= car.batteryDrain
+	car.distance += car.speed
+}
 
 // TODO: define the 'DisplayDistance() string' method
+func (car Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %v meters", car.distance)
+}
 
 // TODO: define the 'DisplayBattery() string' method
+func (car Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %v%%", car.battery)
+}
 
 // TODO: define the 'CanFinish(trackDistance int) bool' method
+func (c *Car) CanFinish(trackDistance int) bool {
+	if c.speed == c.batteryDrain {
+		return trackDistance <= c.battery
+	}
+	return (trackDistance / c.speed * c.batteryDrain) <= c.battery
+}
