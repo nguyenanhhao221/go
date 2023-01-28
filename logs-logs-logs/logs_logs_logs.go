31f9b9a053701f9c97f139115c53b9b25f7adccd
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2023-01-28 18:12:46 +0700

    finish

--- logs-logs-logs/logs_logs_logs.go
+++ logs-logs-logs/logs_logs_logs.go
@@ -1,5 +1,7 @@
 package logs
 
+import "unicode/utf8"
+
 var tableLookUp = map[rune]string{}
 
 // Application identifies the application emitting the given log.
@@ -20,11 +22,20 @@ func Application(log string) string {
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	// panic("Please implement the Replace() function")
+	runes := []rune(log)
+	for index, rune := range runes {
+		if rune == oldRune {
+			runes[index] = newRune
+		}
+	}
+	return string(runes)
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	// panic("Please implement the WithinLimit() function")
+	runesInLog := utf8.RuneCountInString(log)
+	return runesInLog <= limit
 }
