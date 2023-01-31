098f4a2c2bbe6d081bd1ead9b2442b0278152b99
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2023-01-31 23:20:11 +0700

    finish exercise for interface

--- airport-robot/airport_robot.go
+++ airport-robot/airport_robot.go
@@ -1,5 +1,37 @@
 package airportrobot
 
+import "fmt"
+
 // Write your code here.
 // This exercise does not have tests for each individual task.
 // Try to solve all the tasks first before running the tests.
+type Greeter interface {
+	LanguageName() string
+	Greet(visitorName string) string
+}
+
+func SayHello(visitorName string, greeter Greeter) string {
+	return fmt.Sprintf("I can speak %v: %v", greeter.LanguageName(), greeter.Greet(visitorName))
+}
+
+type Italian struct {
+}
+
+func (i Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (i Italian) Greet(visitorName string) string {
+	return fmt.Sprintf("Ciao %s!", visitorName)
+}
+
+type Portuguese struct {
+}
+
+func (i Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (i Portuguese) Greet(visitorName string) string {
+	return fmt.Sprintf("Olá %s!", visitorName)
+}
